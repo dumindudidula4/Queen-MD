0cb3065a24e7743b16391f545d8be7df6122294b
dumindudidula4 <123971830+dumindudidula4@users.noreply.github.com>
2023-02-04 16:44:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ global.APIKeys = {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 global.session = process.env.SESSION_ID || 'PUT HERE',
 global.ownername = process.env.OWNER_NAME === undefined ? "👿𝗗𝗨𝗠𝗜𝗬𝗔⃤𝗕𝗢𝗬🇱🇰" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94771901094' : process.env.OWNER_NUMBER
+let fake = process.env.OWNER_NUMBER === undefined ? '94761676948' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
